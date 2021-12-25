56f2d10059ecf04447cf4fa25f8731883a0712b7
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-25 14:37:49 +0530

    Added rendering news from api and showing spinner

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -5,12 +5,12 @@ export default class Newsitem extends Component {
         const {title, description, urlToImage, url} = this.props;
         return (
             <div className="col-sm-6 col-md-4 col-lg-3">
-                <div className="card" style={{width: '100%'}}>
-                    <img src={urlToImage} className="card-img-top" alt="..."/>
+                <div className="card my-2" style={{height: "450px", width: '100%'}}>
+                    <img src={urlToImage} style={{height:"250px", objectFit:"cover"}} className="card-img-top" alt="No Image Available"/>
                     <div className="card-body">
                     <h5 className="card-title">{title.length<45?title:(title.slice(0, 45)+"...")}</h5>
                     <p className="card-text">{description === null?"No Description Available":(description.length<80?description:(description.slice(0, 80)+"..."))}</p>
-                    <a href={url} target="_blank" className="btn btn-primary">Read More</a>
+                        <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
