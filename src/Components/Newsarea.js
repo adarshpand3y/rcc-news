f40aef95e802b21865af5605b33ca9f9b9181676
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-26 17:56:21 +0530

    Revamped news item

--- src/Components/Newsarea.js
+++ src/Components/Newsarea.js
@@ -17,26 +17,30 @@ export default class Newsarea extends Component {
         const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fb006d7d5f3841b28dc197c4bc9e3ceb";
         const data = await fetch(url);
         const parsedData = await data.json();
-        this.setState({ totalResults: parsedData.totalResults, articles: parsedData.articles, totalNumberOfPages: Math.ceil(this.state.totalResults/20), loading: false});
+        this.setState({ totalResults: parsedData.totalResults, articles: parsedData.articles, totalNumberOfPages: Math.ceil(this.state.totalResults / 20), loading: false });
     }
 
     render() {
         return (
             <div className="container my-3">
-                <h2 className="text-center">RCC News - Top Headlines</h2>
+                <h1 className="text-center">RCC News - Top Headlines</h1>
                 {this.state.loading ? <div className="d-flex justify-content-center">
                     <div className="spinner-border text-primary my-4" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </div>
-                </div>:""}
-                <div className="row">
-                    {this.state.articles.map((element) => {
-                        return <Newsitem title={element.title}
+                </div> : ""}
+                {this.state.articles.map((element) => {
+                    return <Newsitem title={element.title}
                         description={element.description}
                         key={element.url}
                         urlToImage={element.urlToImage}
-                        url={element.url} />
-                    })}
+                        url={element.url}
+                        source={element.source}
+                        date={element.publishedAt} />
+                })}
+                <div className="d-flex justify-content-between">
+                    <button type="button" className="btn btn-primary">&laquo; Previous</button>
+                    <button type="button" className="btn btn-primary">Next &raquo;</button>
                 </div>
             </div>
         )
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
     render() {
-        const {title, description, urlToImage, url} = this.props;
+        const { title, description, urlToImage, url, source, date } = this.props;
         return (
-            <div className="col-sm-6 col-md-4 col-lg-3">
-                <div className="card my-2" style={{height: "450px", width: '100%'}}>
-                    <img src={urlToImage} style={{height:"250px", objectFit:"cover"}} className="card-img-top" alt="Img Unavailable"/>
-                    <div className="card-body">
-                    <h5 className="card-title">{title.length<45?title:(title.slice(0, 45)+"...")}</h5>
-                    <p className="card-text">{description === null?"No Description Available":(description.length<80?description:(description.slice(0, 80)+"..."))}</p>
-                        <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
+            <>
+                <div className="w-100 card my-4" style={{ padding: "10px" }}>
+                    <div className="row">
+                        <div className="col-4">
+                            <img src={urlToImage} style={{ height: "100%", width: "100%", objectFit: "cover" }} className="card-img-top" alt="Img Unavailable" />
+                        </div>
+                        <div className="col-8">
+                            <h3>{title}</h3>
+                            <span className="badge rounded-pill bg-primary">{source["name"]}</span>
+                            <p className="card-text">{description === null ? "No Description Available" : description}</p>
+                            <p className="card-text"><small className="text-muted">{new Date(date).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })} IST</small></p>
+                            <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
