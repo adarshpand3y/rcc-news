de52c675ee7afe31d9f44028194f1852ad92e8f1
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-26 19:59:50 +0530

    Updated UI

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -8,10 +8,10 @@ export default class Newsitem extends Component {
                 <div className="w-100 card my-4" style={{ padding: "10px" }}>
                     <div className="row">
                         <div className="col-4">
-                            <img src={urlToImage} style={{ height: "100%", width: "100%", objectFit: "cover" }} className="card-img-top" alt="Img Unavailable" />
+                            <img src={urlToImage} style={{ height: "100%", width: "100%", objectFit: "cover", border: "1px solid #dfdfdf" }} className="card-img-top" alt="Img Unavailable" />
                         </div>
                         <div className="col-8">
-                            <h3>{title}</h3>
+                            <h4>{title}</h4>
                             <span className="badge rounded-pill bg-primary">{source["name"]}</span>
                             <p className="card-text">{description === null ? "No Description Available" : description}</p>
                             <p className="card-text"><small className="text-muted">{new Date(date).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })} IST</small></p>
