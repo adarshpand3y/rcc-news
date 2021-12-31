ba844b6110da2336044f84ea6cb1fb69b8b96b7e
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-31 12:43:41 +0530

    Changed display changing trigger button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,9 +14,6 @@ const Navbar = (props) => {
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <a role="button" className="nav-link" onClick={props.changeDisplay}>Switch to {props.displayAsList ? "Card" : "List"} View</a>
-                        </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Browse by Category
@@ -32,9 +29,8 @@ const Navbar = (props) => {
                             </ul>
                         </li>
                     </ul>
-                    <div className={`d-flex`} onClick={props.switchThemeProp}>
-                        <div className={`py-1 px-2 border rounded  border rounded border-${props.theme === 'light' ? " dark" : "light"}`}>
-
+                    <div className={`d-flex`}>
+                        <div className={`py-1 px-2 border rounded  border rounded border-${props.theme === 'light' ? "dark" : "white"} me-2`} onClick={props.switchThemeProp}>
                             {props.theme === 'light' ?
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-moon-fill" viewBox="0 0 16 16">
                                     <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
@@ -44,6 +40,18 @@ const Navbar = (props) => {
                                 </svg>
                             }
                         </div>
+
+                        <div className={`py-1 px-2 border rounded  border rounded border-${props.theme === 'light' ? "dark" : "white"}`} onClick={props.changeDisplay}>
+                        {props.displayAsList ? <svg style={{ filter: `${props.theme === 'light'?"invert(0)":"invert(1)"}` }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                            <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+                        </svg> :
+                            <svg style={{ filter: `${props.theme === 'light'?"invert(0)":"invert(1)"}` }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-task" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z" />
+                                <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z" />
+                                <path fillRule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z" />
+                            </svg>}
+                            </div>
                     </div>
                 </div>
             </div>
